96cbdde7c2a60871ee6b1defdf0b93d5c3cc3045
Transoman <vasiag10@gamil.com>
2017-01-25 18:57:44 +0200

    Add new file

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		pug = require('gulp-pug'),
+		autoprefixer = require('gulp-autoprefixer'),
+		browserSync = require('browser-sync').create();
+
+
+var dirs = {
+	'styleDir' : 'app/sass/**/*.sass',
+	'pugDir' : 'app/pug/page/*.pug'
+};
+
+
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: "app"
+		},
+		notify: false
+	});
+});
+
+gulp.task('styles', function() {
+	return gulp.src(dirs.styleDir)
+	.pipe(sass({
+		outputStyle: 'expanded',
+		includePaths: require('node-bourbon').includePaths
+	}).on('error', sass.logError))
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+			cascade: false
+	}))
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('template', function() {
+	return gulp.src(dirs.pugDir)
+	.pipe(pug({
+		pretty: true
+	}))
+	.pipe(gulp.dest('app'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('watch', ['browser-sync', 'template', 'styles'], function() {
+	gulp.watch(dirs.styleDir, ['styles']);
+	gulp.watch(dirs.pugDir, ['template']);
+});
+
+gulp.task('default', ['watch']);
